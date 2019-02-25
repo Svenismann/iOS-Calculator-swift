2ad8ccc6a939a0e067e381c95a74760a0f7edbcc
Sven <s.forstner@me.com>
2019-02-25 15:27:55 +0100

    first commit

---
+++ Calculator/DeviceIdentifier.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceIdentifier.swift
+//  Calculator
+//
+//  Created by Sven Forstner on 24.02.19.
+//  Copyright © 2019 Sven Forstner. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/orangeButton.swift
@@ -0,0 +1,21 @@
+//
+//  orangeButton.swift
+//  Calculator
+//
+//  Created by Sven Forstner on 24.02.19.
+//  Copyright © 2019 Sven Forstner. All rights reserved.
+//
+
+import UIKit
+
+class orangeButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
